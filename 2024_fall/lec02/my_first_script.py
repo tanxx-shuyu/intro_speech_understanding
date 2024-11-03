b73fac23e9e7798f69766955c2eb561eb405c5ba
shuyuz-tanxx <837779366@qq.com>
2024-11-03 15:20:09 +0900

    Create my_first_script.py

---
+++ 2024_fall/lec02/my_first_script.py
@@ -0,0 +1,12 @@
+print("Hello world!")
+
+"""
+This is a script that I am creating as part of lecture 2 in the Speech Understanding course.
+"""
+
+print(5+6)
+print(9-4)
+print(9/3)
+print(5**3)
+print(13%5)
+print(13//5)
